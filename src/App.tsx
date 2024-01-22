bcf099136821766c80aed4413db7659e8d790e85
apipkurniawan <davitaja92@gmail.com>
2024-01-23 00:30:33 +0700

    create authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Refine } from '@refinedev/core';
+import { Authenticated, Refine } from '@refinedev/core';
 import {
   ThemedLayoutV2,
   notificationProvider,
@@ -6,32 +6,71 @@ import {
   RefineThemes,
 } from '@refinedev/antd';
 import routerBindings, {
+  CatchAllNavigate,
   NavigateToResource,
   UnsavedChangesNotifier,
 } from '@refinedev/react-router-v6';
 import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom';
 
 import { ConfigProvider } from 'antd';
+import axios from 'axios';
 import '@refinedev/antd/dist/reset.css';
 
 // data-provider
-import { dataProvider } from './data-provider';
-// import dataProvider from '@refinedev/simple-rest';
-// import { dataProvider } from './rest-data-provider';
+import { dataProvider } from './providers/data-provider';
 
 // view
 import { BlogPostList } from './pages/blog_posts/list';
-import { BlogPostEdit } from './pages/blog_posts/Edit';
+import { BlogPostEdit } from './pages/blog_posts/edit';
 import { BlogPostShow } from './pages/blog_posts/show';
 import { BlogPostCreate } from './pages/blog_posts/create';
+
+// auth
+import createAuthRefreshInterceptor from 'axios-auth-refresh';
+import { authProvider } from './providers/authProvider';
+import { AuthPage } from './pages/auth';
+
+const API_URL = 'https://api.fake-rest.refine.dev';
+const axiosInstance = axios.create();
+
+// Function that will be called to refresh authorization
+const refreshAuthLogic = (failedRequest: {
+  response: { config: { headers: { [x: string]: string } } };
+}) =>
+  axiosInstance
+    .post(`${API_URL}/auth/token/refresh`)
+    .then((tokenRefreshResponse) => {
+      localStorage.setItem('token', tokenRefreshResponse.data.token);
+
+      failedRequest.response.config.headers['Authorization'] =
+        'Bearer ' + tokenRefreshResponse.data.token;
+
+      return Promise.resolve();
+    });
+
+// Instantiate the interceptor
+createAuthRefreshInterceptor(axiosInstance, refreshAuthLogic);
+
+axiosInstance.interceptors.request.use((config) => {
+  // Retrieve the token from local storage
+  const token = JSON.parse(localStorage.getItem('auth') || '');
+  // Check if the header property exists
+  if (config.headers) {
+    // Set the Authorization header if it exists
+    config.headers['Authorization'] = `Bearer ${token}`;
+  }
+
+  return config;
+});
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
       <ConfigProvider theme={RefineThemes.Blue}>
         <Refine
+          authProvider={authProvider}
           routerProvider={routerBindings}
-          dataProvider={dataProvider('https://api.fake-rest.refine.dev')}
+          dataProvider={dataProvider(API_URL)}
           notificationProvider={notificationProvider}
           resources={[
             {
@@ -51,9 +90,14 @@ const App: React.FC = () => {
           <Routes>
             <Route
               element={
-                <ThemedLayoutV2>
-                  <Outlet />
-                </ThemedLayoutV2>
+                <Authenticated
+                  key={1}
+                  fallback={<CatchAllNavigate to='/login' />}
+                >
+                  <ThemedLayoutV2>
+                    <Outlet />
+                  </ThemedLayoutV2>
+                </Authenticated>
               }
             >
               <Route
@@ -66,6 +110,34 @@ const App: React.FC = () => {
                 <Route path='edit/:id' element={<BlogPostEdit />} />
                 <Route path='create' element={<BlogPostCreate />} />
               </Route>
+            </Route>
+            <Route
+              element={
+                <Authenticated key={2} fallback={<Outlet />}>
+                  <NavigateToResource />
+                </Authenticated>
+              }
+            >
+              <Route path='/login' element={<AuthPage type='login' />} />
+              <Route path='/register' element={<AuthPage type='register' />} />
+              <Route
+                path='/forgot-password'
+                element={<AuthPage type='forgotPassword' />}
+              />
+              <Route
+                path='/update-password'
+                element={<AuthPage type='updatePassword' />}
+              />
+            </Route>
+            <Route
+              element={
+                <Authenticated key={3} fallback={<Outlet />}>
+                  <ThemedLayoutV2>
+                    <Outlet />
+                  </ThemedLayoutV2>
+                </Authenticated>
+              }
+            >
               <Route path='*' element={<ErrorComponent />} />
             </Route>
           </Routes>
